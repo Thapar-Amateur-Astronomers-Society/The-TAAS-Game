23a6fc7f8c92ec413352784cdc6678dcfd1de6f8
ikansh <46121461+Ikansh@users.noreply.github.com>
2024-08-29 00:45:04 +0530

    Made main menu more descriptive.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
